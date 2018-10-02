36f77111a5f7556ef10d77ca1e77b9aa5a8db0a5
Hilnanda Ardiansyah <ardikeren75@gmail.com>
2018-10-02 16:28:16 +0700

    first commit

--- app.module.ts
+++ app.module.ts
@@ -9,6 +9,7 @@ import { TugasComponent } from './tugas/tugas.component';
 import { Tugas3Component } from './tugas3/tugas3.component';
 import { KuisHeaderComponent } from './kuis-header/kuis-header.component';
 import { PegawaiComponent } from './pegawai/pegawai.component';
+import { KuisIsiComponent } from './kuis-isi/kuis-isi.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { PegawaiComponent } from './pegawai/pegawai.component';
     TugasComponent,
     Tugas3Component,
     KuisHeaderComponent,
-    PegawaiComponent
+    PegawaiComponent,
+    KuisIsiComponent
   ],
   imports: [
     BrowserModule,
